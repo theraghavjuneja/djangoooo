4e697b210398488cf27fb5543438492d8b2dd344
RAGHAV JUNEJA <raghavjuneja2391@gmail.com>
2025-02-16 09:38:38 +0530

    Added endpoints Home & About using FBV and CBV

--- revision/myapp/views.py
+++ revision/myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views import View
+from django.views.generic import TemplateView
 # Create your views here.
+def home(request):
+    return HttpResponse("Hey, You are Welcomed To The Home Page!!!")
+def about(request):
+    return HttpResponse("Hey, this is the about page? Ok")
+class HomeView(TemplateView):
+    template_name='home.html'
+class AboutView(View):
+    def get(self,request):
+        return HttpResponse("This page is using a CBV")
