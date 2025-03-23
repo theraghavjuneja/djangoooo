c8b1c016673bc591f426efb0b9b9a4422ad83938
RAGHAV JUNEJA <raghavjuneja2391@gmail.com>
2025-03-23 10:01:47 +0530

    Connected crud operations

--- django-chat-room-final-app/chatroom/base/urls.py
+++ django-chat-room-final-app/chatroom/base/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.home,name='home'),
-    path('room/<int:pk>/',views.room,name='room')
+    path('room/<int:pk>/',views.room,name='room'),
+    path('create-room/', views.createRoom, name='create-room'),
+    path('update-room/<int:pk>',views.updateRoom,name='update-room'),
+    path('delete-room/<int:pk>',views.deleteRoom,name='delete-room')
 ]
--- django-chat-room-final-app/chatroom/base/views.py
+++ django-chat-room-final-app/chatroom/base/views.py
@@ -1,12 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Room
-
+from .forms import RoomForm
 def home(request):
     rooms=Room.objects.all()
     context={'rooms':rooms}
+    print(context)
     return render(request,'base/home.html',context)
 def room(request,pk):
     room=Room.objects.get(id=pk) # get the room having id=pk
     context={'room':room}
     return render(request,'base/room.html',context)
+def createRoom(request):
+    form=RoomForm()
+    if request.method=='POST':
+        print(form)
+        form=RoomForm(request.POST)
+        print(form)
+        if form.is_valid():
+            # form.save() will automatically save the room
+            # provided that RoomForm is a valid model having a class meta
+            form.save()
+            return redirect ('home') # Redirect used since I want to send the user to other page
+    context={'form':form}
+    return render(request,'base/room_form.html',context) # I want to render this page
+def updateRoom(request,pk):
+    '''
+    pk: To specify what room are we updating 
+    - we need to prefill the data, unlike createRoom
+    For that we use instance=room
+    '''
+    room=Room.objects.get(id=pk) #get the room id=pk
+    form=RoomForm(instance=room)
+    if request.method=='POST':
+        form=RoomForm(request.POST,instance=room) #need to specify instance so that django can validate
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form':form}
+    return render(request,'base/room_form.html',context)
+def deleteRoom(request,pk):
+    room=Room.objects.get(id=pk)
+    if request.method=='POST':
+        room.delete()
+        return redirect('home')
+    return render(request,'base/delete.html',{'obj':room})
